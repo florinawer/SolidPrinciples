8fc13b9d0f407236124be1dc0bf4b028f2f8a396
flo <kviatan365@gmail.com>
2021-12-23 13:51:07 +0100

    Inicial Commit

---
+++ BagMVC/Controllers/_BaseController.cs
@@ -0,0 +1,29 @@
+﻿using BagLib;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace BagMVC.Controllers
+{
+    public abstract class _BaseController : Controller
+    {
+        protected readonly BagContext _context;
+
+        protected IConfiguration _configuration;
+
+        protected int CurrentUserId { get; set; } = 1;
+
+
+        public _BaseController(BagContext context)
+        {
+            _context = context;
+        }
+
+        public _BaseController(BagContext context, IConfiguration configuration)
+        {
+            _context = context;
+            _configuration = configuration;
+        }
+
+
+    }
+}
